7acc27cad55f48e52beae049212e8064f4830d95
Detavious Cutts <dippydip26@gmail.com>
2017-07-25 12:33:23 -0400

    Our first commit as hitters.

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var buttonCount = 0
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        
+        print(buttonCount)
+        
+        if buttonCount >= 10 {view.backgroundColor = UIColor.green
+            
+            myLabel.text = "You hit it more than 10 times"}
+        
+    
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
